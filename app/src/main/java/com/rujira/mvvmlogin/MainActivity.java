9874af6a5857d5f19e389ac5df64fc0618fb9d41
Rujira Kongsomran <biwberry@outlook.com>
2019-12-07 16:53:18 +0700

    You need rebuild project after done this step

--- app/src/main/java/com/rujira/mvvmlogin/MainActivity.java
+++ app/src/main/java/com/rujira/mvvmlogin/MainActivity.java
@@ -1,14 +1,40 @@
 package com.rujira.mvvmlogin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.rujira.mvvmlogin.Interface.LoginResultCallBacks;
+import com.rujira.mvvmlogin.databinding.ActivityMainBinding;
+import com.rujira.mvvmlogin.viewmodel.LoginViewModel;
+import com.rujira.mvvmlogin.viewmodel.LoginViewModelFactory;
+
+import es.dmoral.toasty.Toasty;
+
+public class MainActivity extends AppCompatActivity implements LoginResultCallBacks {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setViewModel(ViewModelProviders.of(this,
+                new LoginViewModelFactory(this))
+                .get(LoginViewModel.class));
+
+    }
+
+    @Override
+    public void onSuccess(String message) {
+        Toasty.success(this, "Login success", Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    @Override
+    public void onError(String message) {
+        Toasty.error(this, "Login error", Toast.LENGTH_SHORT)
+                .show();
     }
 }
